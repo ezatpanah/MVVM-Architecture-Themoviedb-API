d97b0dbfa3edbf6d6ce937404050bac766f5e8cc
ezatpanah <ezatpanah@gmail.com>
2022-12-08 12:01:06 +0200

    fix baseurl

--- app/src/main/java/com/ezatpanah/themoviedb_api_mvvm/utils/Constants.kt
+++ app/src/main/java/com/ezatpanah/themoviedb_api_mvvm/utils/Constants.kt
@@ -2,6 +2,7 @@ package com.ezatpanah.themoviedb_api_mvvm.utils
 
 object Constants {
     const val API_KEY = "***"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
     const val NETWORK_TIMEOUT = 60L
